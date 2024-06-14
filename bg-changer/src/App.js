d20a462a65a30b4eaeb7310a96f742b2cf1b4890
devanshu-73 <devanshuchauhan732000@gmail.com>
2024-06-14 21:36:10 +0530

    Web Files

---
+++ bg-changer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Count from './Count'
+import Bgcolor from './Bgcolor';
+const App = () => {
+  return (
+    <div>
+      {/* <Count /> */}
+      <Bgcolor />
+    </div>
+  )
+}
+
+export default App
